a7f986618c0a12e29dc71121980e6f9c564dc5e8
GeorgeD615 <st095277@student.spbu.ru>
2023-11-23 20:39:34 +0300

    add program files

---
+++ Program.cs
@@ -0,0 +1,71 @@
+﻿namespace HeapSortTesting
+{
+    
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            uint numOfBaseOperations = 0;
+            int[] arr = RandomInputGenerator.GenerateRandomNumArray(1024, 0, 1000);
+            numOfBaseOperations += HeapSort(arr);
+            Console.WriteLine($"Sorted, number of base operations:{numOfBaseOperations}");
+        }
+
+        static uint HeapSort(int[] array)
+        {
+            uint numOfOperations = 0;
+
+            int n = array.Length;
+
+            for (int i = n / 2 - 1; i >= 0; i--)
+            {
+                numOfOperations += Heapify(array, n, i);
+            }
+
+            for (int i = n - 1; i >= 0; i--)
+            {
+                int temp = array[0];
+                array[0] = array[i];
+                array[i] = temp;
+                ++numOfOperations;
+
+                numOfOperations += Heapify(array, i, 0);
+            }
+
+            return numOfOperations;
+        }
+
+        static uint Heapify(int[] array, int n, int i)
+        {
+            uint numOfOperations = 0;
+
+            int largest = i;
+            int left = 2 * i + 1;
+            int right = 2 * i + 2;
+
+            if (left < n && array[left] > array[largest])
+            {
+                ++numOfOperations;
+                largest = left;
+            }
+
+            if (right < n && array[right] > array[largest])
+            {
+                ++numOfOperations;
+                largest = right;
+            }
+
+            if (largest != i)
+            {
+                int temp = array[i];
+                array[i] = array[largest];
+                array[largest] = temp;
+                ++numOfOperations;
+
+                numOfOperations += Heapify(array, n, largest);
+            }
+
+            return numOfOperations;
+        }
+    }
+}
---
+++ RandomInputGenerator.cs
@@ -0,0 +1,18 @@
+﻿namespace HeapSortTesting
+{
+    public static class RandomInputGenerator
+    {
+        public static int[] GenerateRandomNumArray(int size, int min_bound, int max_bound)
+        {
+            Random random = new Random();
+            int[] array = new int[size];
+
+            for (int i = 0; i < size; i++)
+            {
+                array[i] = random.Next(min_bound, max_bound); 
+            }
+
+            return array;
+        }
+    }
+}
